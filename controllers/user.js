87184b877d0d2cd0f9b789baeaf3a15471eeba17
killerstar1234 <mmitchmmares@gmail.com>
2022-10-19 22:47:29 -0500

    adding user edit page

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-
+const cookieParser = require('cookie-parser');
 
 exports.quick = (req, res) => {
 
@@ -16,6 +16,7 @@ exports.quick = (req, res) => {
 
     }).catch(err => {
         if(err) {
+            res.status(400).render('quick');
             console.log(err);
         }
     })
@@ -24,6 +25,27 @@ exports.quick = (req, res) => {
 
 exports.edit = (req, res) => {
 
-    // code...
+    const email = req.cookies['email'];
+
+    axios.get(`http://www.mitch.redhawks.us/?email=${email}`).then(results => {
+        
+
+        if(results.data) {
+            const subject = results.data.profile
+            res.status(200).render('edit', {profile: subject});
+        } else {
+            res.status(200).render('edit', {profile: 'You Have Nothing In Your Profile'})
+        }
+
+
+    
+
+}).catch(err => {
+        if(err) {
+            res.status(400).render('edit', {message: 'err'});
+            console.log(err);
+        }
+    })
+
 
 }
