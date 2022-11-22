30be9683f83a50f7cc8635cf84727c79402442ee
= <=>
2022-11-22 09:19:44 -0600

    admin view

--- controllers/admin.js
+++ controllers/admin.js
@@ -3,7 +3,8 @@ const axios = require('axios');
 exports.view = (req, res) => {
     axios.get(`http://www.mitch.redhawks.us/?all=true`).then(result => {
         const data = result.data;
-        console.log(data);
-        return res.json(data);
+        return res.render('admin', {
+            data
+        });
     })
 }
